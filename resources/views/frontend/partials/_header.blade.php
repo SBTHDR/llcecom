713d89678f1243b75dd5c775d40df8b1d01dec76
SBTHDR <subrata.halder13d@gmail.com>
2021-07-11 21:07:26 +0600

    add frontend template

---
+++ resources/views/frontend/partials/_header.blade.php
@@ -0,0 +1,46 @@
+<header>
+    <div class="collapse bg-dark" id="navbarHeader">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 col-md-7 py-4">
+                    <h4 class="text-white">Categories</h4>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="text-white" style="text-decoration: none">Food</a></li>
+                        <li><a href="#" class="text-white" style="text-decoration: none">Drinks</a></li>
+                        <li><a href="#" class="text-white" style="text-decoration: none">Others</a></li>
+                        <li><a href="#" class="text-white" style="text-decoration: none">Baby Toys</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4 offset-md-1 py-4">
+                    <h4 class="text-white">Menu</h4>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="text-white" style="text-decoration: none">Cart</a></li>
+
+                        @guest()
+                            <li><a href="#" class="text-white" style="text-decoration: none">Login</a></li>
+                            <li><a href="#" class="text-white" style="text-decoration: none">Create an Account</a></li>
+                        @endguest
+
+                        @auth()
+                            <li><a href="#" class="text-white" style="text-decoration: none">My Profile</a></li>
+                            <li><a href="#" class="text-white" style="text-decoration: none">Log Out</a></li>
+                        @endauth
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="navbar navbar-dark bg-dark shadow-sm">
+        <div class="container">
+            <a href="#" class="navbar-brand d-flex align-items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"  stroke-width="2" class="bi bi-cart4 me-2" viewBox="0 0 16 16">
+                    <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+                </svg>
+                <strong>{{ config('app.name') }}</strong>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </div>
+</header>
