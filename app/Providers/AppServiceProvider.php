eb11e7796735ad4fd8105c8ee4cf976b9c1c1181
SBTHDR <subrata.halder13d@gmail.com>
2021-07-11 23:38:48 +0600

    products display

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Category;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Paginator::useBootstrap();
+
         $categories = Category::select(['name', 'slug'])->where('category_id', null)->get();
 
         View()->share('categories', $categories);
